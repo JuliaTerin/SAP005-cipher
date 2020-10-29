d72433b208834d42d793c6f1246a185d1c8f893f
Julia Terin <jucterin@gmail.com>
2020-10-29 10:23:42 -0300

    Fundo do site e primeiro texto

--- cipher.js
+++ cipher.js
@@ -1,3 +1,8 @@
+//src/cipher.js: aqui você deve implementar o objeto cipher, o qual já está exportado no boilerplate. Este objeto (cipher) deve conter dois métodos:
+//cipher.encode(offset, string): offset é o número de posições que queremos mover para a direita no alfabeto e string é a mensagem (texto) que queremos cifrar.
+//cipher.decode(offset, string): offset é o número de posições que queremos mover para a esquerda no alfabeto e string é a mensagem (texto) que queremos decifrar.
+
+
 const cipher = {
   // ...
 };
