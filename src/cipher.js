c82940e392890df6bd18586fb156cecf8ec0d0d9
Julia Terin <33783277+JuliaTerin@users.noreply.github.com>
2021-03-08 08:59:00 -0300

    Update cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,3 @@
-//src/cipher.js: aqui você deve implementar o objeto cipher, o qual já está exportado no boilerplate. Este objeto (cipher) deve conter dois métodos:
-//cipher.encode(offset, string): offset é o número de posições que queremos mover para a direita no alfabeto e string é a mensagem (texto) que queremos cifrar.
-//cipher.decode(offset, string): offset é o número de posições que queremos mover para a esquerda no alfabeto e string é a mensagem (texto) que queremos decifrar.
 
 const cipher = {
  encode: function(offset, codificar){
@@ -8,12 +5,12 @@ const cipher = {
      throw new TypeError()
    }
    else{
-   let MsgReturn = "";
-   let alphabetSize = 26;
-   let firstAscii = 65;
-   for(let cont = 0; cont < codificar.length; cont ++) {
+   const MsgReturn = "";
+   const alphabetSize = 26;
+   const firstAscii = 65;
+   for(const cont = 0; cont < codificar.length; cont ++) {
       const CodA = codificar.charCodeAt (cont);
-    let codificado = ((CodA - firstAscii + offset) % alphabetSize) + firstAscii;
+    const codificado = ((CodA - firstAscii + offset) % alphabetSize) + firstAscii;
     if (CodA <= 64 || CodA >= 91) {
     codificado = CodA;
     } MsgReturn += String.fromCharCode(codificado);
@@ -26,12 +23,12 @@ const cipher = {
      throw new TypeError()
 
   }else{
-   let MsgReturn = "";
-   let alphabetSize = 26;
-   let firstAscii = 65;    
-   for(let cont = 0; cont < decodifica.length; cont ++) {
+   const MsgReturn = "";
+   const alphabetSize = 26;
+   const firstAscii = 65;    
+   for(const cont = 0; cont < decodifica.length; cont ++) {
       const CodB = decodifica.charCodeAt (cont);
-    let recodificado = ((CodB - firstAscii - (offset % alphabetSize) + alphabetSize) % alphabetSize) + firstAscii;
+    const recodificado = ((CodB - firstAscii - (offset % alphabetSize) + alphabetSize) % alphabetSize) + firstAscii;
     if (CodB <= 64 || CodB >= 91) {
     recodificado = CodB;
     } MsgReturn += String.fromCharCode(recodificado);
